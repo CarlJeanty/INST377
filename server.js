4af43a8efa3858ccb56f55e30e0fde05ea02b91c
Carl Jeanty <CarlJeanty@users.noreply.github.com>
2020-10-09 22:38:59 -0400

    lab 5

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.route('/api')
     
     console.log('POST request detected');
     console.log('Form data in res.body', req.body)
-    res.send('hello world')
+    res.send('Carl Jeanty')
   });
 
 app.listen(port, () => {
