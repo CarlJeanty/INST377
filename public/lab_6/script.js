315e8219875b94f2472f20895a86648a0b5b3086
Carl Jeanty <CarlJeanty@users.noreply.github.com>
2020-10-24 21:03:49 -0400

    d

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,10 @@
+
 // You may wish to find an effective randomizer function on MDN.
+function getRandomIntInclusive(min,max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function range(int) {
   const arr = [];
@@ -8,15 +14,16 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
-  if (a[key] < b[key]) {
+function sortFunction(org, compare, key) {
+  if (org[key] < compare[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  } if (org[key] > compare[key]) {
     return 1;
   }
   return 0;
 }
-function
+
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -27,29 +34,33 @@ document.body.addEventListener('submit', async (e) => {
     },
     body: JSON.stringify(form)
   })
+    
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      if(document.querySelector('.flex-inner')){
-        document.querySelector('.flex-inner').remove();
-      }
-      const newArr = range(10);
-      const newArr2 = newArr.map(()=>{
-        const number =getRandomIntInclusive(0,243);
-        return fromServer[number];
-      });
-      const reverseList = newArr2.sort((a,b)=> sortByKey(a,b,'name'));
-      const ol =document.createElement('ol');
-      ul.className = 'flex-inner';
-      $('form').prepend(ol);
-     
-      reverseList.forEach((el,i)=>{
-        const li = document.createElement('li');
-        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
-        $(li).append(`<bel for=${el.code}>${el.name}</label>`);
-        $(ol).append(li);
-      });
-      Math.random(newArr2)
-    //  console.log('fromServer', fromServer);
+     console.log(fromServer)
+     if (document.querySelector('.flex-inner')){
+      document.querySelector('.flex-inner').remove();
+    }; 
+    const  newArr = range(10);
+    const newArr2 = newArr.map(() => {
+    const number = getRandomIntInclusive(0,243);
+      return fromServer[number];
+    }); 
+    const revL = newArr2.sort((org,compare) => sortFunction(compare,org,'name'));
+    const ol = document.createElement('ol');
+    ol.className = 'flex-inner';
+    $('form').prepend(ol);
+
+    revL.forEach((el,i) => {
+      li = document.createElement('li');
+      $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+      $(li).append(`<label for=${el.code}>${el.name}</label>`);
+      $(ol).append(li);
+    });
+    
+
+    
+      //console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
 });
